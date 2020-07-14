b9459801230bc6dd51f870a9ccb8a1749b9b3c93
gtutone <67984002+gtutone@users.noreply.github.com>
2020-07-13 22:50:25 -0700

    Class setup

---
+++ Python Projects/Python projects/Week5_Fex_Prime generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 22:12:54 2020
+
+@author: gabrieltutone
+"""
+
+class Primes(object):
+    
+    def __init__(self):
+        self.counter = 0
+        self.numbers = []
+        self.primes = []
+        
+    def genPrimes(self):
+        while True:
+            counter =+ 1
+            numbers.append(counter)
+            yield counter
+
+primesNums = 0
+
+print(type(primesNums))
+
+primesNums = Primes()
+
+print(type(primesNums))
+
